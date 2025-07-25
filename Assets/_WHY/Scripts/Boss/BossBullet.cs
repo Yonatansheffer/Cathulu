abbb9894f6299beb0576335a84a9e60ff210991e
Yonatansheffer <yonatan.sheffer@mail.huji.ac.il>
2025-07-25 15:01:21 +0300

    Rotated spell gun

--- Assets/_WHY/Scripts/Boss/BossBullet.cs
+++ Assets/_WHY/Scripts/Boss/BossBullet.cs
@@ -7,6 +7,12 @@ namespace _WHY.Scripts.Boss
     public class BossBullet : WHYBaseMono, IPoolable
     {
         private static readonly int Hit = Animator.StringToHash("Hit");
+        private Rigidbody2D _rb;
+        
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
 
         public void Reset()
         {
@@ -22,17 +28,19 @@ namespace _WHY.Scripts.Boss
 
         private void OnTriggerEnter2D(Collider2D other)
         {
+            _rb.linearVelocity = Vector2.zero;
             animator.SetTrigger(Hit);
-        }
-
-        public void ReturnToPool()
-        {
-            BossBulletPool.Instance.Return(this);
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
+            _rb.linearVelocity = Vector2.zero;
             animator.SetTrigger(Hit);
+        }
+        
+        public void ReturnToPool()
+        {
+            BossBulletPool.Instance.Return(this);
         }
     }
 }
--- Assets/_WHY/Scripts/Weapons/GunAim.cs
+++ Assets/_WHY/Scripts/Weapons/GunAim.cs
@@ -14,14 +14,11 @@ namespace Weapons
         private void Update()
         {
             if (mainCamera == null) return;
-
             Vector3 mouseScreenPos = Input.mousePosition;
             Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, mainCamera.nearClipPlane));
             mouseWorldPos.z = 0f;
-
             Vector3 direction = mouseWorldPos - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
             transform.rotation = Quaternion.Euler(0, 0, angle-90);
         }
     }
